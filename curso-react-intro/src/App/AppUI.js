e982816f0cd26cc8bdcff329cf653a4b6d452880
unknown <jcan2412digital1@gmail.com>
2024-01-04 18:22:26 -0500

    primer commit

---
+++ curso-react-intro/src/App/AppUI.js
@@ -0,0 +1,55 @@
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+
+function AppUI({
+    loading,
+    error,
+    completedTodos,
+    totalTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+}) {
+    return (
+        <>
+          <TodoCounter 
+          completed={completedTodos} 
+          total={totalTodos}
+          />
+          <TodoSearch 
+             searchValue={searchValue}
+             setSearchValue={setSearchValue}
+          />
+     
+           <TodoList>
+            {loading && <p>Estamos Cargando ...</p>}
+            {error && <p>Desesperate, hubo un 
+            error !!...</p>}
+            {(!loading && searchedTodos.length === 
+            0) && <p>!Crea tu primer TODO!!.</p>}
+
+
+             {searchedTodos.map(todo =>(
+             <TodoItem 
+               key={todo.text} 
+               text={todo.text}
+               completed={todo.completed}
+               onComplete={() => completeTodo(todo.
+              text)}
+               onDelete= {() => deleteTodo(todo.
+              text)}
+             />
+             ))}
+           </TodoList>
+           
+           <CreateTodoButton />
+       </>
+       );
+}
+
+export { AppUI };
